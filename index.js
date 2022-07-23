7f5a57638358e84eb32a4945775d228ee8338657
Patricia McPhee <hi@patriciamcphee.com>
2022-07-23 14:06:48 -0700

    change port

--- index.js
+++ index.js
@@ -283,5 +283,5 @@ app.use((err, req, res, next) => {
 // listen for requests
 const port = process.env.PORT || 8080
 app.listen(port, '0.0.0.0', () => {
-  console.log('Thank you for listening to P.O.R.T. ' + port + '!!');
+  console.log('Thank you for listening to P.O.R.T. ' + port);
 });
