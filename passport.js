485b958a63bcf8ac12595fc9dcecc0be07b52469
Patricia McPhee <hi@patriciamcphee.com>
2022-07-25 13:55:22 -0700

    troubleshoot jwt

--- passport.js
+++ passport.js
@@ -33,7 +33,7 @@ passport.use(new LocalStrategy({
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
+  secretOrKey: 'gQMTKHxbNQHCX63m'
 }, (jwtPayload, callback) => {
   return Users.findById(jwtPayload._id)
     .then((user) => {
