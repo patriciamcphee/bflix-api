112765a8332c4873e878a19e12ab5e205a2686f4
Patricia McPhee <88200052+patriciamcphee@users.noreply.github.com>
2022-09-06 12:40:19 -0700

    troubleshoot cors

--- index.js
+++ index.js
@@ -28,6 +28,20 @@ const cors = require('cors');
 //allow requests from all origins
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 /*
 let allowedOrigins = ['http://localhost:1234'];
 
